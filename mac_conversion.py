ff72f4f877414f87b17e1c7694321ae31ac90a7c
javier <jbenavi2@asu.edu>
2018-05-01 02:59:56 -0700

    format lines

--- mac_conversion.py
+++ mac_conversion.py
@@ -1,40 +1,6 @@
-# import sys
-#
-# months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-# arguments = sys.argv
-# print(len(sys.argv))
-# print(arguments)
-#
-# if arguments[1] == '-T':
-#     conversion = 'time'
-# else:
-#     conversion = 'date'
-#
-# if arguments[2] == '-f':
-#     filename = arguments[3]
-# else:
-#     prefix, hex_value = arguments[3].split('x')
-#
-# little_endian = hex_value[2:] + hex_value[0:2]
-# binary = format(int(little_endian, 16), '0>16b')
-#
-# if conversion == 'date':
-#     year = binary[0:7]
-#     year = int(year, 2) + 1980
-#
-#     month = binary[7:11]
-#     month = months[int(month, 2) - 1]
-#
-#     day = binary[11:]
-#     day = int(day, 2)
-#
-#     print("Date: " + month + " " + str(day) + ", " + str(year))
-# elif conversion == 'time':
-
 import argparse
 import sys
 import datetime as dt
-
 
 
 def macCommands():
@@ -79,6 +45,7 @@ def bin2time(binary):
     time = dt.time(hour, min, sec)
     return time.strftime("Time: %I:%M:%S %p")
 
+
 def main():
     command = macCommands()
 
@@ -104,7 +71,6 @@ def main():
         print(bin2time(hex2bin(hex_input)))
     elif namespace.D:
         print(bin2date(hex2bin(hex_input)))
-
 
 
 if __name__ == '__main__':
