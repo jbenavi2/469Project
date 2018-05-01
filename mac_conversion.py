068916e3dd0958c2b96419c931217bfecbf6c199
javier <jbenavi2@asu.edu>
2018-05-01 02:54:40 -0700

    mac complete

--- mac_conversion.py
+++ mac_conversion.py
@@ -1,32 +1,111 @@
+# import sys
+#
+# months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+# arguments = sys.argv
+# print(len(sys.argv))
+# print(arguments)
+#
+# if arguments[1] == '-T':
+#     conversion = 'time'
+# else:
+#     conversion = 'date'
+#
+# if arguments[2] == '-f':
+#     filename = arguments[3]
+# else:
+#     prefix, hex_value = arguments[3].split('x')
+#
+# little_endian = hex_value[2:] + hex_value[0:2]
+# binary = format(int(little_endian, 16), '0>16b')
+#
+# if conversion == 'date':
+#     year = binary[0:7]
+#     year = int(year, 2) + 1980
+#
+#     month = binary[7:11]
+#     month = months[int(month, 2) - 1]
+#
+#     day = binary[11:]
+#     day = int(day, 2)
+#
+#     print("Date: " + month + " " + str(day) + ", " + str(year))
+# elif conversion == 'time':
+
+import argparse
 import sys
+import datetime as dt
 
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-arguments = sys.argv
-print(len(sys.argv))
-print(arguments)
 
-if arguments[1] == '-T':
-    conversion = 'time'
-else:
-    conversion = 'date'
 
-if arguments[2] == '-f':
-    filename = arguments[3]
-else:
-    prefix, hex_value = arguments[3].split('x')
+def macCommands():
+    # parse input out of sys.argv
+    parser = argparse.ArgumentParser(prog='MAC Conversion')
 
-little_endian = hex_value[2:] + hex_value[0:2]
-binary = format(int(little_endian, 16), '0>16b')
+    # mutual exclusive.  Either T or D
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-T', action='store_true')
+    group.add_argument('-D', action='store_true')
 
-if conversion == 'date':
-    year = binary[0:7]
-    year = int(year, 2) + 1980
+    # mutual exclusive.  Either f or h
+    group2 = parser.add_mutually_exclusive_group(required=True)
+    group2.add_argument('-f', metavar='filename')
+    # can't use -h as it's already used for help by default by argparse
+    group2.add_argument('-hex', metavar='hex_value')
 
-    month = binary[7:11]
-    month = months[int(month, 2) - 1]
+    return parser
 
-    day = binary[11:]
-    day = int(day, 2)
 
-    print("Date: " + month + " " + str(day) + ", " + str(year))
-elif conversion == 'time':
+def hex2bin(string):
+    # remove 0x from hex input
+    string = string[2:]
+    binary = bin(int(string, 16))
+    # remove 0b from binary output
+    binary = binary[2:]
+    return binary
+
+
+def bin2date(binary):
+    year = int(binary[0:6], 2) + 1980
+    month = int(binary[6:10], 2)
+    day = int(binary[10:], 2)
+    date = dt.date(year, month, day)
+    return date.strftime("Date: %b %d, %Y")
+
+
+def bin2time(binary):
+    hour = int(binary[0:4], 2)
+    min = int(binary[4:10], 2)
+    sec = int(binary[10:15], 2) * 2
+    time = dt.time(hour, min, sec)
+    return time.strftime("Time: %I:%M:%S %p")
+
+def main():
+    command = macCommands()
+
+    command.print_usage()
+
+    """test conversions"""
+    # sample_date = '0x4f42'
+    # print(bin2date(hex2bin(sample_date)))
+    #
+    # sample_time = '0x53f6'
+    # print(bin2time(hex2bin(sample_time)))
+
+    namespace = command.parse_args()
+
+    if namespace.f is not None:
+        fopen = open(namespace.f, 'r')
+        hex_input = fopen.readline()
+        fopen.close()
+    else:
+        hex_input = namespace.hex
+
+    if namespace.T:
+        print(bin2time(hex2bin(hex_input)))
+    elif namespace.D:
+        print(bin2date(hex2bin(hex_input)))
+
+
+
+if __name__ == '__main__':
+    main()
